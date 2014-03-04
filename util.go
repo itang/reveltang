652bb4e3aba53dd0f490303ad674808f640c512a
itang <live.tang@gmail.com>
2014-03-04 13:46:06 +0800

    update

---
+++ util.go
@@ -0,0 +1,16 @@
+package reveltang
+
+import (
+	"fmt"
+
+	"github.com/robfig/revel"
+	"github.com/itang/gotang"
+)
+
+// 获取应用配置， 如果未配置，则panic
+func ForceGetConfig(key string) string {
+	value, exists := revel.Config.String(key)
+	gotang.Assert(exists, fmt.Sprintf("Missing revel app config for key: %s", key))
+
+	return value
+}
